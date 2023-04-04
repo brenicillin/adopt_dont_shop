842afe302f202d39155c4c0f0073cad5eda65d9e
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-04 15:38:13 -0400

    condense validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,11 +2,5 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
-  validates :description, presence: true
-  validates :status, presence: true
-end 
+  validates :name, :address, :city, :state, :zip, :description, :status, presence: true
+end
