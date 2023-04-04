9429550a1461a4b7a608ab944b7c92bf4f857d80
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-04 15:39:23 -0400

    update pet app update method

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,23 +1,13 @@
 class PetApplicationsController < ApplicationController
   def create
     application = Application.find(params[:application_id])
-    # pets = application.pets.distinct
-    # if pets.length == 1
-    #   params[:pet_id] = pets.first.id
-    #   PetApplication.create!(pet_application_params)
-    # else
-      # pets.each do |pet|
-      #   params[:pet_id] = pet.id
-      #   PetApplication.create!(pet_application_params)
-      # end
-    # end
     application.update(status: "Pending")
     redirect_to "/applications/#{params[:application_id]}"
   end
 
   def status
-    pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
-    pet_application.update(status: params[:status])
+    @pet_application = PetApplication.find_by(pet_id: params[:pet_id], application_id: params[:id])
+    @pet_application.update(status: params[:status])
     redirect_to admin_application_path(params[:id])
   end
 
